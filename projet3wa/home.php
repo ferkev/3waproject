aa3ff29ed0defec3182dbcf609674e881b56aa06
ferkev <34628639+ferkev@users.noreply.github.com>
2018-01-09 13:47:45 +0100

    Update home.php

--- projet3wa/home.php
+++ projet3wa/home.php
@@ -11,7 +11,7 @@ if(!empty($_POST)){
 	$content= $_POST['content'];
 	$email=$_POST['email'];
 
-	if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+	if(filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($title) && !empty($content) && strlen($title)<=100 && strlen($content)<=500 && strlen($email)<=100){
 
 		$query = $pdo->prepare("
 		INSERT INTO contact (Title, Message, Email, CreationTimestamp )
